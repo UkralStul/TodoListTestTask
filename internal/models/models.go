2946051dbe46fc9138078d6f0baf612171489ca2
UkralStul <stoyanov03@mail.ru>
2025-03-12 16:00:46 +0300

    models.go time fix

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,10 +1,12 @@
 package models
 
+import "time"
+
 type Task struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description,omitempty"`
-	Status      string `json:"status"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description,omitempty"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
